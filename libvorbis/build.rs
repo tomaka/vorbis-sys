75a6e734bbfef49b3f05b52c26e76dfa53738c30
Tomaka17 <pierre.krieger1708@gmail.com>
2014-11-02 11:14:04 +0100

    Add libvorbis file and tweak build script

--- libvorbis/build.rs
+++ libvorbis/build.rs
@@ -8,34 +8,47 @@ fn main() {
         include_directories: vec![
             root.join("include"),
             std::os::getenv("DEP_OGG_INCLUDE").unwrap(),
-        ]
+        ],
+        definitions: vec![
+            ("_USRDLL".to_string(), None),
+            ("LIBVORBIS_EXPORTS".to_string(), None)
+        ],
+        .. std::default::Default::default()
     }
 
     println!("cargo:include={}", root.join("include"));
 
     let libfiles_root = root.join("lib");
 
-    gcc::compile_library("libvorbis.a", &[
-        libfiles_root.join("analysis.c"),
-        libfiles_root.join("bitrate.c"),
-        libfiles_root.join("block.c"),
-        libfiles_root.join("codebook.c"),
-        libfiles_root.join("envelope.c"),
-        libfiles_root.join("floor0.c"),
-        libfiles_root.join("floor1.c"),
-        libfiles_root.join("info.c"),
-        libfiles_root.join("lookup.c"),
-        libfiles_root.join("lpc.c"),
-        libfiles_root.join("lsp.c"),
-        libfiles_root.join("mapping0.c"),
-        libfiles_root.join("mdct.c"),
-        libfiles_root.join("psy.c"),
-        libfiles_root.join("registry.c"),
-        libfiles_root.join("res0.c"),
-        libfiles_root.join("sharedbook.c"),
-        libfiles_root.join("smallft.c"),
-        libfiles_root.join("synthesis.c"),
-        libfiles_root.join("vorbisenc.c"),
-        libfiles_root.join("window.c"),
-    ]);
+    if std::os::args().find(|s| s.as_slice() == "vorbis").is_some() {
+        gcc::compile_library("libvorbis.a", &[
+            libfiles_root.join("analysis.c"),
+            libfiles_root.join("bitrate.c"),
+            libfiles_root.join("block.c"),
+            libfiles_root.join("codebook.c"),
+            libfiles_root.join("envelope.c"),
+            libfiles_root.join("floor0.c"),
+            libfiles_root.join("floor1.c"),
+            libfiles_root.join("info.c"),
+            libfiles_root.join("lookup.c"),
+            libfiles_root.join("lpc.c"),
+            libfiles_root.join("lsp.c"),
+            libfiles_root.join("mapping0.c"),
+            libfiles_root.join("mdct.c"),
+            libfiles_root.join("psy.c"),
+            libfiles_root.join("registry.c"),
+            libfiles_root.join("res0.c"),
+            libfiles_root.join("sharedbook.c"),
+            libfiles_root.join("smallft.c"),
+            libfiles_root.join("synthesis.c"),
+            libfiles_root.join("vorbisenc.c"),
+            libfiles_root.join("window.c"),
+        ]);
+    }
+
+    if std::os::args().find(|s| s.as_slice() == "vorbisfile").is_some() {
+        gcc::compile_library("libvorbisfile.a", &[
+            libfiles_root.join("vorbisfile.c"),
+        ]);
+    }
 }
