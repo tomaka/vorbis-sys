118773ae8e50b552cc5bc8bae12bea0a0c2965b3
Pierre Krieger <pierre.krieger1708@gmail.com>
2015-02-12 13:43:56 +0100

    Update for gcc, rust and libc

--- libvorbis/build.rs
+++ libvorbis/build.rs
@@ -7,46 +7,35 @@ fn main() {
         Err(..) => {}
     };
 
-    let root = Path::new(std::os::getenv("CARGO_MANIFEST_DIR").unwrap())
-        .join("libvorbis");
+    let root = Path::new(std::env::var_string("CARGO_MANIFEST_DIR").unwrap())
+                    .join("libvorbis");
 
-    let config = gcc::Config {
-        include_directories: vec![
-            root.join("include"),
-            root.join("lib"),
-            Path::new(std::os::getenv("DEP_OGG_INCLUDE").unwrap()),
-        ],
-        definitions: vec![
-            ("_USRDLL".to_string(), None),
-            ("LIBVORBIS_EXPORTS".to_string(), None)
-        ],
-        .. std::default::Default::default()
-    };
-
-    println!("cargo:include={}", root.join("include").as_str().unwrap());
-    println!("cargo:src={}", root.join("lib").as_str().unwrap());
-
-    gcc::compile_library("libvorbis.a", &config, &[
-        "libvorbis/lib/analysis.c",
-        "libvorbis/lib/bitrate.c",
-        "libvorbis/lib/block.c",
-        "libvorbis/lib/codebook.c",
-        "libvorbis/lib/envelope.c",
-        "libvorbis/lib/floor0.c",
-        "libvorbis/lib/floor1.c",
-        "libvorbis/lib/info.c",
-        "libvorbis/lib/lookup.c",
-        "libvorbis/lib/lpc.c",
-        "libvorbis/lib/lsp.c",
-        "libvorbis/lib/mapping0.c",
-        "libvorbis/lib/mdct.c",
-        "libvorbis/lib/psy.c",
-        "libvorbis/lib/registry.c",
-        "libvorbis/lib/res0.c",
-        "libvorbis/lib/sharedbook.c",
-        "libvorbis/lib/smallft.c",
-        "libvorbis/lib/synthesis.c",
-        "libvorbis/lib/vorbisenc.c",
-        "libvorbis/lib/window.c",
-    ]);
+    gcc::Config::new()
+                .file("libvorbis/lib/analysis.c")
+                .file("libvorbis/lib/bitrate.c")
+                .file("libvorbis/lib/block.c")
+                .file("libvorbis/lib/codebook.c")
+                .file("libvorbis/lib/envelope.c")
+                .file("libvorbis/lib/floor0.c")
+                .file("libvorbis/lib/floor1.c")
+                .file("libvorbis/lib/info.c")
+                .file("libvorbis/lib/lookup.c")
+                .file("libvorbis/lib/lpc.c")
+                .file("libvorbis/lib/lsp.c")
+                .file("libvorbis/lib/mapping0.c")
+                .file("libvorbis/lib/mdct.c")
+                .file("libvorbis/lib/psy.c")
+                .file("libvorbis/lib/registry.c")
+                .file("libvorbis/lib/res0.c")
+                .file("libvorbis/lib/sharedbook.c")
+                .file("libvorbis/lib/smallft.c")
+                .file("libvorbis/lib/synthesis.c")
+                .file("libvorbis/lib/vorbisenc.c")
+                .file("libvorbis/lib/window.c")
+                .define("_USRDLL", None)
+                .define("LIBVORBIS_EXPORTS", None)
+                .include(root.join("include"))
+                .include(root.join("lib"))
+                .include(Path::new(std::env::var_string("DEP_OGG_INCLUDE").unwrap()))
+                .compile("libvorbis.a");
 }
